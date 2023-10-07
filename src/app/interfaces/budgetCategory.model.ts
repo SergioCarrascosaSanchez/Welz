3190434c234a23ed7aa39e08ff24d2e4a5dbda22
SergioCarrascosaSanchez <sercarsa11@gmail.com>
2023-10-07 09:20:50 +0200

    add BudgetCategory interface

---
+++ src/app/interfaces/budgetCategory.model.ts
@@ -0,0 +1,5 @@
+export interface BudgetCategory {
+  name: string;
+  max: number;
+  color: string;
+}
--- src/app/interfaces/transaction.model.ts
+++ src/app/interfaces/transaction.model.ts
@@ -1,6 +1,8 @@
+import { BudgetCategory } from './budgetCategory.model';
+
 export interface Transaction {
   description: string;
-  budgetCategory: string; //(catergorias definidas por el usuario)
+  budgetCategory: BudgetCategory;
   account: string;
   value: number;
   date: Date;
