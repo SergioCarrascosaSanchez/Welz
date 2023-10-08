09297ffb61339c11b9ff2095a5b7ab36d7f75381
SergioCarrascosaSanchez <sercarsa11@gmail.com>
2023-10-08 13:51:01 +0200

    add dynamic css styling to Card

--- src/app/components/card/card.component.cy.ts
+++ src/app/components/card/card.component.cy.ts
@@ -1,11 +1,37 @@
 import { CardComponent } from './card.component';
 
 describe('CardComponent', () => {
-  it('should render content', () => {
+  it('normal card should render content', () => {
     const content = 'Test Content';
-    cy.mount(`<app-card [count]="100">${content}</app-card>`, {
+    cy.mount(`<app-card>${content}</app-card>`, {
       declarations: [CardComponent],
     });
     cy.contains(content);
+  });
+
+  it('normal card should have card class', () => {
+    const content = 'Test Content';
+    cy.mount(`<app-card>${content}</app-card>`, {
+      declarations: [CardComponent],
+    });
+    cy.get('[role="region"]').should('have.class', 'card');
+    cy.get('[role="region"]').should('not.have.class', 'subcard');
+  });
+
+  it('subcard card should render content', () => {
+    const content = 'Test Content';
+    cy.mount(`<app-card type="subcard">${content}</app-card>`, {
+      declarations: [CardComponent],
+    });
+    cy.contains(content);
+  });
+
+  it('subcard card should have subcard class', () => {
+    const content = 'Test Content';
+    cy.mount(`<app-card type="subcard">${content}</app-card>`, {
+      declarations: [CardComponent],
+    });
+    cy.get('[role="region"]').should('have.class', 'subcard');
+    cy.get('[role="region"]').should('not.have.class', 'card');
   });
 });
