319a06e6c793ec9e8c5812393a15faabace4b0c6
SergioCarrascosaSanchez <sercarsa11@gmail.com>
2023-10-09 10:10:42 +0200

    create budget route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { TransactionPageComponent } from './pages/transactions/transactions-page.component';
+import { BudgetComponent } from './pages/budget/budget.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'budget', component: HomeComponent },
+  { path: 'budget', component: BudgetComponent },
   {
     path: 'transactions',
     component: TransactionPageComponent,
