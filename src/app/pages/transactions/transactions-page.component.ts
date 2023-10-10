5af6edba908d3d9c6cacb9e4af7313f26b425624
SergioCarrascosaSanchez <sercarsa11@gmail.com>
2023-10-10 12:40:29 +0200

    transactions page get information form dataService

--- src/app/pages/transactions/transactions-page.component.ts
+++ src/app/pages/transactions/transactions-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Transaction } from 'src/app/interfaces/transaction.model';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-transaction-page',
@@ -7,62 +8,14 @@ import { Transaction } from 'src/app/interfaces/transaction.model';
   styleUrls: ['./transactions-page.component.css'],
 })
 export class TransactionPageComponent {
-  @Input() transactions: Transaction[] = [
-    {
-      description: 'Compra de comestibles',
-      budgetCategory: { name: 'Alimentacion', max: 1000, color: 'red' },
-      account: 'Cuenta principal',
-      value: 50.25,
-      date: new Date('2023-10-06'),
-    },
-    {
-      description: 'Pago de factura de electricidad',
-      budgetCategory: { name: 'Servicios', max: 1000, color: 'blue' },
-      account: 'Cuenta principal',
-      value: 120.0,
-      date: new Date('2023-10-05'),
-    },
-    {
-      description: 'Comida rápida',
-      account: 'Cuenta principal',
-      budgetCategory: {
-        name: 'Entretenimiento',
-        max: 1000,
-        color: 'green',
-      },
-      value: 120.0,
-      date: new Date('2023-10-05'),
-    },
-    {
-      description: 'Compra de gasolina',
-      budgetCategory: { name: 'Transporte', max: 500, color: 'orange' },
-      account: 'Cuenta principal',
-      value: 40.0,
-      date: new Date('2023-10-04'),
-    },
-    {
-      description: 'Pago de factura de teléfono',
-      budgetCategory: { name: 'Comunicaciones', max: 500, color: 'purple' },
-      account: 'Cuenta principal',
-      value: 60.0,
-      date: new Date('2023-10-03'),
-    },
-    {
-      description: 'Cena en un restaurante',
-      budgetCategory: { name: 'Alimentacion', max: 1000, color: 'red' },
-      account: 'Cuenta principal',
-      value: 75.5,
-      date: new Date('2023-10-02'),
-    },
-    {
-      description: 'Compra de boletos de cine',
-      budgetCategory: { name: 'Entretenimiento', max: 200, color: 'green' },
-      account: 'Cuenta principal',
-      value: 30.0,
-      date: new Date('2023-10-01'),
-    },
-  ];
+  transactions: Transaction[];
   emptyTransactionsMessage = emptyTransactions;
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit() {
+    this.transactions = this.dataService.getTransactions();
+  }
 }
 
 export const emptyTransactions = 'No hay ninguna transacción.';
