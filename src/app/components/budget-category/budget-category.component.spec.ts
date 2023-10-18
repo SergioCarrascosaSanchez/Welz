e953835b7660f18ce89078aa40e8c30d158f1674
SergioCarrascosaSanchez <sercarsa11@gmail.com>
2023-10-18 21:10:58 +0200

    migrate budget category tests

---
+++ src/app/components/budget-category/budget-category.component.spec.ts
@@ -0,0 +1,104 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BudgetCategoryComponent } from './budget-category.component';
+import { BudgetCategoryItemComponent } from '../budget-category-item/budget-category-item.component';
+import { DebugElement } from '@angular/core';
+import { CardComponent } from '../card/card.component';
+import { BadgeComponent } from '../badge/badge.component';
+import { MoneyFormatPipe } from 'src/app/pipes/money-format.pipe';
+import { TransactionComponent } from '../transaction/transaction.component';
+import { DataService } from 'src/app/services/data.service';
+import { Injectable } from '@angular/core';
+
+describe('BudgetCategoryComponent', () => {
+  let component: BudgetCategoryComponent;
+  let fixture: ComponentFixture<BudgetCategoryComponent>;
+  let debugElement: DebugElement;
+
+  it('should create', () => {
+    configureTestBed();
+    expect(component).toBeTruthy();
+  });
+
+  it('should render title', () => {
+    configureTestBed();
+    const title = 'TitleTest';
+    component.title = title;
+    fixture.detectChanges();
+
+    expect(debugElement.nativeElement.textContent).toContain(title);
+  });
+
+  it('should render list of categories', () => {
+    configureTestBed();
+    const budgetCategory1 = {
+      name: 'TestingBudgetCategory1',
+      max: 128376,
+      color: 'red',
+    };
+    const budgetCategory2 = {
+      name: 'TestingBudgetCategory2',
+      max: 9012376,
+      color: 'blue',
+    };
+    const budgetCategory3 = {
+      name: 'TestingBudgetCategory3',
+      max: 12376.223,
+      color: 'green',
+    };
+
+    component.categories = [budgetCategory1, budgetCategory2, budgetCategory3];
+
+    fixture.detectChanges();
+
+    expect(debugElement.nativeElement.textContent).toContain(
+      budgetCategory1.name
+    );
+    expect(debugElement.nativeElement.textContent).toContain(
+      new MoneyFormatPipe().transform(budgetCategory1.max)
+    );
+
+    expect(debugElement.nativeElement.textContent).toContain(
+      budgetCategory2.name
+    );
+    expect(debugElement.nativeElement.textContent).toContain(
+      new MoneyFormatPipe().transform(budgetCategory2.max)
+    );
+
+    expect(debugElement.nativeElement.textContent).toContain(
+      budgetCategory3.name
+    );
+    expect(debugElement.nativeElement.textContent).toContain(
+      new MoneyFormatPipe().transform(budgetCategory3.max)
+    );
+  });
+
+  const configureTestBed = () => {
+    TestBed.configureTestingModule({
+      declarations: [
+        BudgetCategoryComponent,
+        BudgetCategoryItemComponent,
+        CardComponent,
+        BadgeComponent,
+        MoneyFormatPipe,
+        TransactionComponent,
+      ],
+      providers: [{ provide: DataService, useClass: EmptyDataServiceMock }],
+    });
+
+    fixture = TestBed.createComponent(BudgetCategoryComponent);
+    component = fixture.componentInstance;
+    debugElement = fixture.debugElement;
+    fixture.detectChanges();
+  };
+});
+
+/* DataServiceMocks */
+
+@Injectable({
+  providedIn: 'root',
+})
+class EmptyDataServiceMock {
+  getTransactionsOfBudgetCategory(s: string) {
+    return [];
+  }
+}
