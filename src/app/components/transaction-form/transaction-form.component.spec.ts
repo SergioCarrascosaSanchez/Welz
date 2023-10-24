e79e83dcdd7fa82d62dea1298a0fe238d66422f5
SergioCarrascosaSanchez <sercarsa11@gmail.com>
2023-10-24 16:50:43 +0200

    update Transaction Form

--- src/app/components/transaction-form/transaction-form.component.spec.ts
+++ src/app/components/transaction-form/transaction-form.component.spec.ts
@@ -1,6 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { TransactionFormComponent } from './transaction-form.component';
+import { By } from '@angular/platform-browser';
+import {
+  TransactionFormComponent,
+  Title,
+  EmptyError,
+  MinAndEmptyError,
+  MinError,
+} from './transaction-form.component';
+import { AlertComponent } from '../alert/alert.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BudgetCategory } from 'src/app/interfaces/budgetCategory.model';
+import { Account } from 'src/app/interfaces/account.model';
+import { DataService } from 'src/app/services/data.service';
 
 describe('TransactionFormComponent', () => {
   let component: TransactionFormComponent;
@@ -8,7 +19,9 @@ describe('TransactionFormComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [TransactionFormComponent]
+      declarations: [TransactionFormComponent, AlertComponent],
+      imports: [ReactiveFormsModule],
+      providers: [{ provide: DataService, useClass: DataServiceMock }],
     });
     fixture = TestBed.createComponent(TransactionFormComponent);
     component = fixture.componentInstance;
@@ -18,4 +31,235 @@ describe('TransactionFormComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render inputs, title and button', () => {
+    expect(fixture.debugElement.nativeElement.textContent).toContain(Title);
+    expect(
+      fixture.debugElement.query(By.css('input[formControlName="description"]'))
+    ).toBeTruthy();
+    expect(
+      fixture.debugElement.query(By.css('input[formControlName="quantity"]'))
+    ).toBeTruthy();
+    expect(
+      fixture.debugElement.query(By.css('select[formControlName="category"]'))
+    ).toBeTruthy();
+    expect(
+      fixture.debugElement.query(By.css('select[formControlName="account"]'))
+    ).toBeTruthy();
+    expect(fixture.debugElement.query(By.css('button'))).toBeTruthy();
+  });
+
+  it('should display correct value of select inputs', () => {
+    expect(fixture.debugElement.nativeElement.textContent).toContain(
+      budgetCategory1.name
+    );
+    expect(fixture.debugElement.nativeElement.textContent).toContain(
+      budgetCategory2.name
+    );
+    expect(fixture.debugElement.nativeElement.textContent).toContain(
+      account1.name
+    );
+    expect(fixture.debugElement.nativeElement.textContent).toContain(
+      account2.name
+    );
+  });
+
+  it('should render error message if all inputs are empty', () => {
+    expect(fixture.debugElement.nativeElement.textContent).not.toContain(
+      EmptyError
+    );
+    const button = fixture.nativeElement.querySelector('button');
+    expect(button).toBeTruthy();
+    button.click();
+    fixture.detectChanges();
+    expect(fixture.debugElement.nativeElement.textContent).toContain(
+      EmptyError
+    );
+  });
+
+  it('should render error message if description is empty', () => {
+    expect(fixture.debugElement.nativeElement.textContent).not.toContain(
+      EmptyError
+    );
+
+    component.transactionForm.controls.quantity.setValue(3);
+    component.transactionForm.controls.account.setValue(account1);
+    component.transactionForm.controls.category.setValue(budgetCategory1);
+
+    const button = fixture.nativeElement.querySelector('button');
+    button.click();
+    fixture.detectChanges();
+
+    expect(fixture.debugElement.nativeElement.textContent).toContain(
+      EmptyError
+    );
+  });
+
+  it('should render error message if quantity is empty', () => {
+    expect(fixture.debugElement.nativeElement.textContent).not.toContain(
+      EmptyError
+    );
+
+    component.transactionForm.controls.description.setValue('Test');
+    component.transactionForm.controls.account.setValue(account1);
+    component.transactionForm.controls.category.setValue(budgetCategory1);
+
+    const button = fixture.nativeElement.querySelector('button');
+    button.click();
+    fixture.detectChanges();
+
+    expect(fixture.debugElement.nativeElement.textContent).toContain(
+      EmptyError
+    );
+  });
+
+  it('should render error message if account is empty', () => {
+    expect(fixture.debugElement.nativeElement.textContent).not.toContain(
+      EmptyError
+    );
+
+    component.transactionForm.controls.description.setValue('Test');
+    component.transactionForm.controls.quantity.setValue(3);
+    component.transactionForm.controls.category.setValue(budgetCategory1);
+
+    const button = fixture.nativeElement.querySelector('button');
+    button.click();
+    fixture.detectChanges();
+
+    expect(fixture.debugElement.nativeElement.textContent).toContain(
+      EmptyError
+    );
+  });
+
+  it('should render error message if category is empty', () => {
+    expect(fixture.debugElement.nativeElement.textContent).not.toContain(
+      EmptyError
+    );
+
+    component.transactionForm.controls.description.setValue('Test');
+    component.transactionForm.controls.quantity.setValue(3);
+    component.transactionForm.controls.account.setValue(account1);
+
+    const button = fixture.nativeElement.querySelector('button');
+    button.click();
+    fixture.detectChanges();
+
+    expect(fixture.debugElement.nativeElement.textContent).toContain(
+      EmptyError
+    );
+  });
+
+  it('should not render error message if no category is empty', () => {
+    expect(fixture.debugElement.nativeElement.textContent).not.toContain(
+      EmptyError
+    );
+
+    component.transactionForm.controls.description.setValue('Test');
+    component.transactionForm.controls.quantity.setValue(3);
+    component.transactionForm.controls.category.setValue(budgetCategory1);
+    component.transactionForm.controls.account.setValue(account1);
+
+    const button = fixture.nativeElement.querySelector('button');
+    button.click();
+    fixture.detectChanges();
+
+    expect(fixture.debugElement.nativeElement.textContent).not.toContain(
+      EmptyError
+    );
+  });
+
+  it('should not render error message if no category is empty', () => {
+    expect(fixture.debugElement.nativeElement.textContent).not.toContain(
+      EmptyError
+    );
+
+    component.transactionForm.controls.description.setValue('Test');
+    component.transactionForm.controls.quantity.setValue(3);
+    component.transactionForm.controls.category.setValue(budgetCategory1);
+    component.transactionForm.controls.account.setValue(account1);
+
+    const button = fixture.nativeElement.querySelector('button');
+    button.click();
+    fixture.detectChanges();
+
+    expect(fixture.debugElement.nativeElement.textContent).not.toContain(
+      EmptyError
+    );
+  });
+
+  it('should not render error message if quantity is 0', () => {
+    expect(fixture.debugElement.nativeElement.textContent).not.toContain(
+      EmptyError
+    );
+
+    component.transactionForm.controls.description.setValue('Test');
+    component.transactionForm.controls.quantity.setValue(0);
+    component.transactionForm.controls.category.setValue(budgetCategory1);
+    component.transactionForm.controls.account.setValue(account1);
+
+    const button = fixture.nativeElement.querySelector('button');
+    button.click();
+    fixture.detectChanges();
+
+    expect(fixture.debugElement.nativeElement.textContent).toContain(MinError);
+  });
+
+  it('should not render error message if quantity is less than 0', () => {
+    expect(fixture.debugElement.nativeElement.textContent).not.toContain(
+      EmptyError
+    );
+
+    component.transactionForm.controls.description.setValue('Test');
+    component.transactionForm.controls.quantity.setValue(-1);
+    component.transactionForm.controls.category.setValue(budgetCategory1);
+    component.transactionForm.controls.account.setValue(account1);
+
+    const button = fixture.nativeElement.querySelector('button');
+    button.click();
+    fixture.detectChanges();
+
+    expect(fixture.debugElement.nativeElement.textContent).toContain(MinError);
+  });
+
+  it('should not render error message if quantity is 0 or less and there is a empty field', () => {
+    expect(fixture.debugElement.nativeElement.textContent).not.toContain(
+      EmptyError
+    );
+
+    component.transactionForm.controls.quantity.setValue(0);
+    component.transactionForm.controls.category.setValue(budgetCategory1);
+    component.transactionForm.controls.account.setValue(account1);
+
+    const button = fixture.nativeElement.querySelector('button');
+    button.click();
+    fixture.detectChanges();
+
+    expect(fixture.debugElement.nativeElement.textContent).toContain(
+      MinAndEmptyError
+    );
+  });
 });
+
+/* DataServiceMocks */
+
+class DataServiceMock {
+  getBudgetCategories() {
+    return [budgetCategory1, budgetCategory2];
+  }
+  getAccounts() {
+    return [account1, account2];
+  }
+}
+
+const budgetCategory1: BudgetCategory = {
+  name: 'Mock1',
+  max: 1000,
+  color: 'red',
+};
+const budgetCategory2: BudgetCategory = {
+  name: 'Mock2',
+  max: 1300,
+  color: 'blue',
+};
+const account1: Account = { name: 'Account1', balance: 1 };
+const account2: Account = { name: 'Account2', balance: 2 };
--- src/app/components/transaction-form/transaction-form.component.ts
+++ src/app/components/transaction-form/transaction-form.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Account } from 'src/app/interfaces/account.model';
 import { BudgetCategory } from 'src/app/interfaces/budgetCategory.model';
 import { DataService } from 'src/app/services/data.service';
+import { EmptyValidator } from 'src/app/validators/empty-validator';
 
 @Component({
   selector: 'app-transaction-form',
@@ -13,14 +14,23 @@ export class TransactionFormComponent {
   constructor(private dataService: DataService) {}
 
   transactionForm = new FormGroup({
-    description: new FormControl<string>(null),
-    quantity: new FormControl<number>(null),
-    category: new FormControl(null),
-    account: new FormControl(null),
+    description: new FormControl<string>(null, [EmptyValidator]),
+    quantity: new FormControl<number>(null, [
+      Validators.min(1),
+      EmptyValidator,
+    ]),
+    category: new FormControl<BudgetCategory>(null, [EmptyValidator]),
+    account: new FormControl<Account>(null, [EmptyValidator]),
   });
+
+  invalid = false;
 
   categories: BudgetCategory[];
   accounts: Account[];
+
+  errors = '';
+
+  title = Title;
 
   ngOnInit() {
     this.categories = this.dataService.getBudgetCategories();
@@ -28,16 +38,56 @@ export class TransactionFormComponent {
   }
 
   onSubmit() {
-    console.log(this.transactionForm.value);
-    this.clearForm();
+    if (!this.transactionForm.invalid && !this.transactionForm.pristine) {
+      this.resetForm();
+      this.invalid = false;
+    } else {
+      this.manageErrors();
+      this.invalid = true;
+    }
   }
 
-  clearForm() {
+  resetForm() {
+    this.errors = '';
+    this.invalid = true;
     this.transactionForm = new FormGroup({
-      description: new FormControl<string>(null),
-      quantity: new FormControl<number>(null),
-      category: new FormControl(null),
-      account: new FormControl(null),
+      description: new FormControl<string>(null, [EmptyValidator]),
+      quantity: new FormControl<number>(null, [
+        Validators.min(1),
+        EmptyValidator,
+      ]),
+      category: new FormControl(null, [EmptyValidator]),
+      account: new FormControl(null, [EmptyValidator]),
     });
   }
+
+  manageErrors() {
+    let errorArray = [];
+    this.errors = '';
+    Object.values(this.transactionForm.controls).forEach(
+      (formControl: FormControl) => {
+        if (formControl.errors) {
+          errorArray = [...errorArray, ...Object.keys(formControl.errors)];
+        }
+      }
+    );
+    if (errorArray.includes('min') && errorArray.includes('emptyValue')) {
+      this.errors = MinAndEmptyError;
+      return;
+    }
+    if (errorArray.includes('min')) {
+      this.errors = MinError;
+      return;
+    }
+    if (errorArray.includes('emptyValue')) {
+      this.errors = EmptyError;
+      return;
+    }
+  }
 }
+
+export const MinAndEmptyError: string =
+  'Debes rellenar todos los campos e incluir una cantidad mayor que cero.';
+export const MinError: string = 'Debes incluir una cantidad mayor que cero.';
+export const EmptyError: string = 'Debes rellenar todos los campos.';
+export const Title: string = 'Nueva transacción';
