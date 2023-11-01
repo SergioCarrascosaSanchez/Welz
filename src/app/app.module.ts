a6b4fc45a401c98404c16641e87b3bd3be841c3d
SergioCarrascosaSanchez <sercarsa11@gmail.com>
2023-11-01 12:11:22 +0100

    register all new components in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CardComponent } from './components/card/card.component';
-import { AccountComponent } from './components/account/account.component';
 import { MoneyFormatPipe } from './pipes/money-format.pipe';
 import { TransactionComponent } from './components/transaction/transaction.component';
 import { BadgeComponent } from './components/badge/badge.component';
@@ -13,20 +12,20 @@ import { TransactionPageComponent } from './pages/transactions/transactions-page
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { BudgetComponent } from './pages/budget/budget.component';
 import { BudgetCategoryComponent } from './components/budget-category/budget-category.component';
-import { BudgetCategoryItemComponent } from './components/budget-category-item/budget-category-item.component';
 import { ModalComponent } from './components/modal/modal.component';
 import { TransactionFormComponent } from './components/transaction-form/transaction-form.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AlertComponent } from './components/alert/alert.component';
 import { BudgetCategoryFormComponent } from './components/budget-category-form/budget-category-form.component';
 import { ColorPickerComponent } from './components/color-picker/color-picker.component';
+import { TransactionsCollapsableComponent } from './components/transactions-collapsable/transactions-collapsable.component';
+import { AccountsComponent } from './components/accounts/accounts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     CardComponent,
-    AccountComponent,
     MoneyFormatPipe,
     TransactionComponent,
     BadgeComponent,
@@ -34,12 +33,13 @@ import { ColorPickerComponent } from './components/color-picker/color-picker.com
     NavbarComponent,
     BudgetComponent,
     BudgetCategoryComponent,
-    BudgetCategoryItemComponent,
     ModalComponent,
     TransactionFormComponent,
     AlertComponent,
     BudgetCategoryFormComponent,
     ColorPickerComponent,
+    TransactionsCollapsableComponent,
+    AccountsComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, FormsModule],
   providers: [],
