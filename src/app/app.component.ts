b06d9436878b0e1477d4679b096503589befc1da
SergioCarrascosaSanchez <sercarsa11@gmail.com>
2023-11-09 18:05:59 +0100

    create main page to contain app component content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,24 +6,4 @@ import { DataService } from './services/data.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'budget-app';
-  username: string;
-  addModalOpen = false;
-  dataLoaded = false;
-  constructor(private dataService: DataService) {}
-
-  ngOnInit() {
-    this.dataService.fetchData();
-    this.username = this.dataService.getUsername();
-    this.dataService.loadedData.subscribe((state) => (this.dataLoaded = state));
-  }
-
-  onAdd() {
-    this.addModalOpen = true;
-  }
-
-  onClose() {
-    this.addModalOpen = false;
-  }
-}
+export class AppComponent {}
---
+++ src/app/pages/main/main.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css'],
+})
+export class MainComponent {
+  title = 'budget-app';
+  username: string;
+  addModalOpen = false;
+  dataLoaded = false;
+  constructor(private dataService: DataService) {}
+
+  ngOnInit() {
+    this.dataService.fetchData();
+    this.username = this.dataService.getUsername();
+    this.dataService.loadedData.subscribe((state) => (this.dataLoaded = state));
+  }
+
+  onAdd() {
+    this.addModalOpen = true;
+  }
+
+  onClose() {
+    this.addModalOpen = false;
+  }
+}
