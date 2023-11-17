11548cbb5236d25df84a0011f83b423fc737d495
SergioCarrascosaSanchez <sercarsa11@gmail.com>
2023-11-17 19:36:59 +0100

    add alerts at main component if there is no accounts or categories

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ALERT_TYPES } from 'src/app/components/alert/alert.component';
+import { Account } from 'src/app/interfaces/account.model';
+import { BudgetCategory } from 'src/app/interfaces/budgetCategory.model';
 import { DataService } from 'src/app/services/data.service';
 
 @Component({
@@ -11,14 +14,28 @@ export class MainComponent {
   username: string = '';
   addModalOpen = false;
   dataLoaded = false;
+  alertType = ALERT_TYPES.danger;
+  accounts: Account[];
+  categories: BudgetCategory[];
   constructor(private dataService: DataService) {}
 
   ngOnInit() {
     this.dataService.fetchData();
+
     this.username = this.dataService.getUsername();
     this.dataService.loadedData.subscribe((state) => {
       this.dataLoaded = state;
       this.username = this.dataService.getUsername();
+
+      if (state) {
+        this.accounts = this.dataService.getAccounts();
+        this.categories = this.dataService.getBudgetCategories();
+
+        this.dataService.dataChange.subscribe(() => {
+          this.accounts = this.dataService.getAccounts();
+          this.categories = this.dataService.getBudgetCategories();
+        });
+      }
     });
   }
 
